e76973978ff4bf815b46bed4192afac149aa3bd3
admin <admin@email.com>
2023-03-03 17:08:36 +0100

    add first page

--- components/CurrentMeteo.js
+++ components/CurrentMeteo.js
@@ -1,42 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import axios from 'axios';
+import datas from '../datas.json';
 
 import * as Location from 'expo-location';
 
 export default function CurrentMeteo() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const API_KEY = "d67a9881ffa6a0cb88d07601ea126648";
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [iconUri, setIconUri] = useState("");
+  const [text, setText] = useState('Waiting..');
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        setErrorMsg('Veillez activer les permissions de la localisation');
         return;
       }
 
-
       try {
-        let location = await Location.getCurrentPositionAsync({});
-        console.log("ici");
+        const location = await Location.getCurrentPositionAsync({});
         setLocation(location);
       }catch(error){
-        console.log(error);
+        console.error(error);
       }
     })();
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+  useEffect(() => {
+    if (currentWeather) {
+      setText(currentWeather.name);
+      setIconUri(`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`);
+    }
+  }, [currentWeather])
+
+  useEffect(() => {
+    if (errorMsg) {
+      setText(errorMsg);
+    } else if (location) {
+      const lat = location.coords.latitude;
+      const lon = location.coords.longitude;
+
+    //   axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=fr&units=metric`)
+    //       .then(response => {
+    //         // iconUri = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+    //         console.log(response.data);
+    //         setCurrentWeather(response.data);
+    //       })
+    //       .catch(error => {
+    //         console.log(error);
+    //          text = "Une erreur est survenue, réessayez plus tard";
+    //       });
+
+      setCurrentWeather(() => datas);
+    }
+  }, [location])
 
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
+      {text && 
+        <Text style={styles.paragraph}>Météo de <Text style={styles.city} >{text}</Text></Text>
+      }
+      {currentWeather &&
+        <Text>{Math.round(currentWeather.main.temp)}°C</Text>
+      }
+      {iconUri != "" && 
+        <Image style={styles.image} source={{ uri: iconUri }} ></Image>
+      }
     </View>
   );
 }
@@ -44,8 +79,17 @@ export default function CurrentMeteo() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "pink",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
   },
+  image: {
+    width: 200,
+    height: 200,
+    margin: 10,
+  },
+  city:{
+    fontWeight: "bold",
+  }
 });
